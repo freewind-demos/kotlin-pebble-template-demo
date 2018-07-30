1925c5a06c6b4e13aa634e01a37a7dee0f2c996b
freewind <nowindlee@gmail.com>
2018-07-30 13:55:01 +0800

    demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,30 @@
 package example
 
+import com.mitchellbosecke.pebble.PebbleEngine
+import com.mitchellbosecke.pebble.loader.ClasspathLoader
+import java.io.StringWriter
+import java.util.HashMap
+
 fun main(args: Array<String>) {
-    println(hello("Kotlin"))
+    val loader = ClasspathLoader()
+    val engine = PebbleEngine.Builder().loader(loader).strictVariables(true).build()
+    val compiledTemplate = engine.getTemplate("hello.html")
+
+    val items = listOf(
+            Item("Item 1", "$19.99", listOf("good", "item", "new"), listOf(Feature("New!"), Feature("Awesome!"))),
+            Item("Item 2", "$29.99", listOf("old", "item", "bad"), listOf(Feature("Old."), Feature("Ugly.")))
+    )
+
+    val context = HashMap<String, Any>()
+    context["items"] = items
+
+    val writer = StringWriter()
+    compiledTemplate.evaluate(writer, context)
+
+    val output = writer.toString()
+    println(output)
 }
 
-fun hello(name: String): String = "Hello, $name!"
+class Feature(val description: String)
+
+class Item(val name: String, val price: String, val keywords: List<String>, val features: List<Feature>)
